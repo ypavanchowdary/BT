c0227c2bc2ae8be204bef3ca5f661810d641850e
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-06-29 05:23:36 -0400

    Created a Class node that holds the tree representations

--- src/com/pavan/yalamanchili/BT.java
+++ src/com/pavan/yalamanchili/BT.java
@@ -18,3 +18,20 @@ public class BT {
 	}
 
 }
+
+
+/* Java program to determine if binary tree is 
+height balanced or not */
+ 
+/* A binary tree node has data, pointer to left child,
+and a pointer to right child */
+class Node 
+{
+ int data;
+ Node left, right;
+ Node(int d) 
+ {
+     data = d;
+     left = right = null;
+ }
+}
